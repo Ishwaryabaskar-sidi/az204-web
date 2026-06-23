4422d6bfaf1802b7625dc0065c9fcb2bd5479010
Ishwarya Baskar <ishubaskar1999@gmail.com>
2026-06-23 10:23:05 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+app.get('/',(req,res)=>{
+    const env =
+    process.env.APP_ENVIRONMENT || 'unset'
+    res.send(`Hello From Ishwarya
+    Environment = ${env}. 
+    Host = ${process.env.WEBSITE_SITE_NAME || 'local'}`
+    );
+})
+// app.get('/',(req,res)=>{
+//    res.send(`Hello From Ishwarya`);})
+app.get('/health',(req,res)=> 
+    res.json({status:'ok',time:new Date().toISOString()
+    })
+);
+app.listen(port,()=> 
+console.log(`Listing on ${port}`));
+
+
+
+
